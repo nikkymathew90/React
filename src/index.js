c09790a6f58ea5c32b45e48136a32125d943216e
Thomas Mathew, Nikky (US - Mumbai) <nthomasmathew@deloitte.com>
2020-07-14 16:58:19 +0530

    TicTacToe using React hooks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//tictactoe using react hooks
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Square = props => {
+  return (
+    <button className="square" onClick={props.clicked}>
+      {props.number}
+    </button>
+  );
+};
+
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(" "));
+  const [isXNext, setisXNext] = useState(true);
+
+  function resetGame() {
+    setSquares(Array(9).fill(" "));
+  }
+
+  function updateSquares(sign, i) {
+    const nextSquares = [...squares];
+    setSquares(nextSquares);
+    nextSquares[i] = isXNext ? "o" : "x";
+    setisXNext(!isXNext);
+  }
+
+  function renderSquare(i) {
+    return (
+      <Square
+        number={squares[i]}
+        clicked={() => {
+          updateSquares("x", i);
+        }}
+      />
+    );
+  }
+
+  const status = isXNext ? "o" : "x";
+
+  return (
+    <div>
+      <div className="status">Next Player : {status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+      <div className="reset" onClick={resetGame}>
+        New Game
+      </div>
+    </div>
+  );
+};
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
