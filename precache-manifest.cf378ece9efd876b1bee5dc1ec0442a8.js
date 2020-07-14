2bf921f3748c19220b9ad176d9009e8ef7fd2263
Thomas Mathew, Nikky (US - Mumbai) <nthomasmathew@deloitte.com>
2020-07-14 16:56:38 +0530

    Updates

---
+++ precache-manifest.cf378ece9efd876b1bee5dc1ec0442a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebefdbb9f27eb25f0574787021c7b5ae",
+    "url": "/tictactoe-hooks/index.html"
+  },
+  {
+    "revision": "55b42c9641f8a5a01a73",
+    "url": "/tictactoe-hooks/static/css/main.33bb6c7b.chunk.css"
+  },
+  {
+    "revision": "d2d5bc5549a431266cff",
+    "url": "/tictactoe-hooks/static/js/2.6530ad7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe-hooks/static/js/2.6530ad7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55b42c9641f8a5a01a73",
+    "url": "/tictactoe-hooks/static/js/main.445a791c.chunk.js"
+  },
+  {
+    "revision": "f20ddfb8ef862ab7a701",
+    "url": "/tictactoe-hooks/static/js/runtime-main.0f9d896d.js"
+  }
+]);
